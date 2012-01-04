8e316d5b909cf2eab4fe004ad10851d370e16f4b
zender <nathan.zender@ramparthosting.com>
2012-01-03 21:42:19 -0500

    Adding date created on comments.

--- grails-app/domain/com/blog/Comment.groovy
+++ grails-app/domain/com/blog/Comment.groovy
@@ -1,12 +1,20 @@
 package com.blog
 
+import java.util.Date;
+
 class Comment {
 
 	String name
 	String comment
+	Date dateCreated
 	
 	static belongsTo = Entry
 	
     static constraints = {
+		name(blank:false, size:0..255)
+		comment(blank:false, size:0..255)
     }
+	def beforeInsert() {
+		dateCreated = new Date()
+	}
 }
