020942abdfbf88f5e93afa9bc857d6688ed26567
Nathan Zender <nathan.zender@ramparthosting.com>
2013-01-09 06:50:01 -0500

    Updating the Datasource to point my mysql server for development so people do not have to also configure an enviornment variable to switch the grails enviornment.

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -13,24 +13,12 @@ hibernate {
 environments {
     development {
         dataSource {
-            dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
-            url = "jdbc:h2:mem:devDb;MVCC=TRUE"
-        }
-    }
-    test {
-        dataSource {
-            dbCreate = "update"
-            url = "jdbc:h2:mem:testDb;MVCC=TRUE"
-        }
-    }
-    production {
-        dataSource {
 			driverClassName = "com.mysql.jdbc.Driver"
 			dialect = "org.hibernate.dialect.MySQL5Dialect"
-			username = ""
-			password = ""
+			username = "<username>"
+			password = "<password>"
             dbCreate = "update"
-			url = "jdbc:mysql://localhost/nuez"
+			url = "jdbc:mysql://<ec2-hostname>/nuez"
             pooled = true
             properties {
                maxActive = -1
@@ -42,6 +30,31 @@ environments {
                testOnReturn=true
                validationQuery="SELECT 1"
             }
+    }
+    test {
+        dataSource {
+            dbCreate = "update"
+            url = "jdbc:h2:mem:testDb;MVCC=TRUE"
         }
+    }
+    production {
+        dataSource {
+			driverClassName = "com.mysql.jdbc.Driver"
+			dialect = "org.hibernate.dialect.MySQL5Dialect"
+			username = "<username>"
+			password = "<password>"
+            dbCreate = "update"
+			url = "jdbc:mysql://<ec2-hostname>/nuez"
+            pooled = true
+            properties {
+               maxActive = -1
+               minEvictableIdleTimeMillis=1800000
+               timeBetweenEvictionRunsMillis=1800000
+               numTestsPerEvictionRun=3
+               testOnBorrow=true
+               testWhileIdle=true
+               testOnReturn=true
+               validationQuery="SELECT 1"
+            }
     }
 }
